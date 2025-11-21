0408d312d6f5dfccc22790801069eb6e2fde2dc5
AMINE <amineelhailaa@gmail.com>
2025-11-21 09:59:59 +0100

    jai fini responsive

--- js/script.js
+++ js/script.js
@@ -684,18 +684,30 @@ function noCardAvailable(){
 function cardOfRooms(objet) {
     const card = document.createElement("div")
     card.className = "roomCards"
-    card.innerHTML = `<div id="${objet.id}" class="relative   rounded-md border-2  border-white max-w-[30%] lg:max-w-[15%] min-w-0 ">
-                            <img src="${objet.img}" class="rounded-sm  w-full " alt="profile">
-                            <svg  class="deleteBtn  cursor-pointer absolute -top-1.5 left-1/2 -translate-x-1/2 w-[30%] aspect-square" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg">
-                                <path d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0"
-                                      fill="#f44336"/>
-                                <path d="m350.273438 320.105469c8.339843 8.34375 8.339843 21.824219 0 30.167969-4.160157 4.160156-9.621094 6.25-15.085938 6.25-5.460938 0-10.921875-2.089844-15.082031-6.25l-64.105469-64.109376-64.105469 64.109376c-4.160156 4.160156-9.621093 6.25-15.082031 6.25-5.464844 0-10.925781-2.089844-15.085938-6.25-8.339843-8.34375-8.339843-21.824219 0-30.167969l64.109376-64.105469-64.109376-64.105469c-8.339843-8.34375-8.339843-21.824219 0-30.167969 8.34375-8.339843 21.824219-8.339843 30.167969 0l64.105469 64.109376 64.105469-64.109376c8.34375-8.339843 21.824219-8.339843 30.167969 0 8.339843 8.34375 8.339843 21.824219 0 30.167969l-64.109376 64.105469zm0 0"
-                                      fill="#fafafa"/>
-                            </svg>
-                            <div class="absolute top-11/12 left-1/2 px-1 transform -translate-x-1/2  min-w-[120%] max-w-14  rounded-xs bg-orange-500 whitespace-nowrap  overflow-hidden flex justify-center font-semibold ">
-                                <h1 class="text-[0.4rem]">${objet.name.split(" ")[0]}</h1>
-                            </div>
-                        </div>`
+    card.innerHTML =
+        // `<div id="${objet.id}" class="relative   rounded-md border-2  border-white max-w-[30%] lg:max-w-[15%] min-w-0 ">
+        //                     <img src="${objet.img}" class="rounded-sm  w-full " alt="profile">
+        //                     <svg  class="deleteBtn  cursor-pointer absolute -top-1.5 left-1/2 -translate-x-1/2 w-[30%] aspect-square" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg">
+        //                         <path d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0"
+        //                               fill="#f44336"/>
+        //                         <path d="m350.273438 320.105469c8.339843 8.34375 8.339843 21.824219 0 30.167969-4.160157 4.160156-9.621094 6.25-15.085938 6.25-5.460938 0-10.921875-2.089844-15.082031-6.25l-64.105469-64.109376-64.105469 64.109376c-4.160156 4.160156-9.621093 6.25-15.082031 6.25-5.464844 0-10.925781-2.089844-15.085938-6.25-8.339843-8.34375-8.339843-21.824219 0-30.167969l64.109376-64.105469-64.109376-64.105469c-8.339843-8.34375-8.339843-21.824219 0-30.167969 8.34375-8.339843 21.824219-8.339843 30.167969 0l64.105469 64.109376 64.105469-64.109376c8.34375-8.339843 21.824219-8.339843 30.167969 0 8.339843 8.34375 8.339843 21.824219 0 30.167969l-64.109376 64.105469zm0 0"
+        //                               fill="#fafafa"/>
+        //                     </svg>
+        //                     <div class="absolute top-11/12 left-1/2 px-1 transform -translate-x-1/2  min-w-[120%] max-w-14  rounded-xs bg-orange-500 whitespace-nowrap  overflow-hidden flex justify-center font-semibold ">
+        //                         <h1 class="text-[0.4rem]">${objet.name.split(" ")[0]}</h1>
+        //                     </div>
+        //                 </div>`
+    `<div id="${objet.id}" class="relative flex gap-[5%]   rounded-sm bg-white border-2  border-white max-w-[80px] md:max-w-[90px] aspect-[3/1] min-w-0 ">
+                                <img src="${objet.img}" class="rounded-sm  " alt="profile">
+                                <svg  class="deleteBtn  cursor-pointer absolute top-1/2 left-full -translate-1/2 w-[20%] aspect-square" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg">
+                                    <path d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0"
+                                          fill="#f44336"/>
+                                    <path d="m350.273438 320.105469c8.339843 8.34375 8.339843 21.824219 0 30.167969-4.160157 4.160156-9.621094 6.25-15.085938 6.25-5.460938 0-10.921875-2.089844-15.082031-6.25l-64.105469-64.109376-64.105469 64.109376c-4.160156 4.160156-9.621093 6.25-15.082031 6.25-5.464844 0-10.925781-2.089844-15.085938-6.25-8.339843-8.34375-8.339843-21.824219 0-30.167969l64.109376-64.105469-64.109376-64.105469c-8.339843-8.34375-8.339843-21.824219 0-30.167969 8.34375-8.339843 21.824219-8.339843 30.167969 0l64.105469 64.109376 64.105469-64.109376c8.34375-8.339843 21.824219-8.339843 30.167969 0 8.339843 8.34375 8.339843 21.824219 0 30.167969l-64.109376 64.105469zm0 0"
+                                          fill="#fafafa"/>
+                                </svg>
+                                <div class="text-[8px] md:text-[9px] text-black flex flex-col "><h1 class="border-b-amber-400 border-b-[1px] lg:border-b-2 w-fit" >${objet.name.split(" ")[0]}</h1><p>${objet.role}</p> </div>
+
+                            </div>`
     verifyArray()
     return card
 }
